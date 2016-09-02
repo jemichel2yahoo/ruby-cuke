b6ebd7c018cf30ef0d33c6bb136753f8cf3ec58c
Jeffrey Michel <jemichel@yahoo.com>
2016-09-01 21:51:20 -0400

    add minimal files

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,12 @@
+
+Given(/^I am on the Cucumber\.js GitHub repository$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I go to the README file$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)" as the page title$/) do |title|
+  #pending # Write code here that turns the phrase above into concrete actions
+end
